62017f81f5a336fbdb473d16f9293845686bc5ec
Márton Boros <martonboros@gmail.com>
2022-06-03 17:08:27 +0300

    clean up flake, remove unnecessary abstraction

--- flake.nix
+++ flake.nix
@@ -48,14 +48,46 @@
         let
           pkgs = nixpkgsFor system;
           pkgs' = nixpkgsFor' system;
-          plutus = import inputs.plutus { inherit system; };
-          src = ./.;
-        in
-        import ./nix/haskell.nix {
-          inherit src inputs pkgs pkgs' system extraSources haskellModules;
-          inherit (bot-plutus-interface) cabalProjectLocal;
-        };
+          project = pkgs.haskell-nix.cabalProject {
+            name = "plutip";
+            src = ./.;
+            compiler-nix-name = "ghc8107";
 
+            shell = {
+              withHoogle = true;
+              exactDeps = true;
+
+              additional = ps: [ ps.bot-plutus-interface ];
+
+              tools.haskell-language-server = "latest";
+
+              nativeBuildInputs = with pkgs'; [
+                # Haskell Tools
+                haskellPackages.fourmolu
+                haskellPackages.cabal-install
+                haskellPackages.cabal-fmt
+                nixpkgs-fmt
+                hlint
+                entr
+                ghcid
+                git
+                fd
+
+                # hls doesn't support preprocessors yet so this has to exist in PATH
+                haskellPackages.record-dot-preprocessor
+
+                # Cardano tools
+                project.hsPkgs.cardano-cli.components.exes.cardano-cli
+                project.hsPkgs.cardano-node.components.exes.cardano-node
+              ];
+            };
+
+            inherit (bot-plutus-interface) cabalProjectLocal;
+            inherit extraSources;
+            modules = haskellModules;
+          };
+        in
+        project;
     in
     {
       inherit extraSources haskellModules;
--- nix/README.md
+++
@@ -1,3 +0,0 @@
-# Nix tools for Plutip
-
-TODO

fb59ec18714fdfebb18a0b75d2ba0f9418bc19fd
Vladimir Kalnitsky <vladimir@mlabs.city>
2022-06-06 22:02:03 +0400

    Kill dead comment

--- plutip-server/Main.hs
+++ plutip-server/Main.hs
@@ -65,28 +65,3 @@ serverOptionsParser =
         <> Options.showDefault
         <> Options.value Nothing
       )
-
-  -- (st, _) <- startCluster def $ do
-  --   w <- addSomeWallet [ toAda 10000 ]
-
-  --   waitSeconds 2 -- let wallet Tx finish
-
-  --   separate
-  --   liftIO $ do
-  --     putStrLn $ "Wallet PKH: " ++ show (walletPkh w)
-  --     putStrLn $ "Wallet mainnet address: " ++ show (mkMainnetAddress w)
-  --   prtNodeRelatedInfo
-  --   separate
-
-  -- putStrLn "Cluster is running. Press Enter to stop."
-  --   >> void getLine
-  -- putStrLn "Stopping cluster"
-
-  -- stopCluster st
-  -- where
-  --   prtNodeRelatedInfo = ReaderT $ \cEnv -> do
-  --     putStrLn $ "Node socket: " <> show (nodeSocket cEnv)
-
-  --   separate = liftIO $ putStrLn "\n------------\n"
-
-  --   toAda = (* 1_000_000)

81af9a4fd942797d3c7e361d95ed864abad1e99c
zmrocze <karolochmanmilarski@gmail.com>
2022-07-12 15:48:19 +0200

    Update update-proposal.

--- cluster-data/update-proposal.sh
+++ cluster-data/update-proposal.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+# Run from inside cluster-data 
+
 # Inputs: $PAYMENT_KEY, $NUM_GENESIS_KEYS, $KEY_DIR, $TESTNET_MAGIC, $PROPOSAL_ARGS
 
 network=("--mainnet")
+# network=("--testnet-magic" "764824073")
 
 # use some faucet address for fee
 PAYMENT_KEY=faucet-addrs/faucet200
@@ -16,6 +19,7 @@ TXIN=$(cardano-cli query utxo --address "$CHANGE_ADDRESS" "${network[@]}" --out-
         | jq -r 'to_entries[0]|.key'
 )
 EPOCH=$(cardano-cli query tip  "${network[@]}" | jq .epoch)
+# ((EPOCH=EPOCH + 1))
 
 PROPOSAL_ARGS=("--protocol-major-version" "7" "--protocol-minor-version" "0")
 
@@ -28,25 +32,49 @@ cardano-cli governance create-update-proposal \
     "${PROPOSAL_ARGS[@]}" \
     "${PROPOSAL_KEY_ARGS[@]}" \
     --out-file update.proposal
-# cardano-cli transaction build \
-#     --tx-in "$TXIN" \
-#     --change-address "$CHANGE_ADDRESS" \
-#     "${network[@]}" \
-#     --out-file tx-proposal.txbody
+
 cardano-cli transaction build \
     --tx-in "$TXIN" \
     --change-address "$CHANGE_ADDRESS" \
     --update-proposal-file update.proposal \
+    --witness-override 3 \
     "${network[@]}" \
     --out-file tx-proposal.txbody
 
 cardano-cli transaction sign \
+    "${network[@]}" \
     --tx-body-file tx-proposal.txbody \
     --out-file tx-proposal.txsigned \
     --signing-key-file $PAYMENT_SIGNING_KEY --address "$CHANGE_ADDRESS" \
-    --signing-key-file genesis-keys/genesis1.skey \
-    # "${SIGNING_ARGS[@]}"
+    --signing-key-file bft-leader.skey
 
-cardano-cli transaction submit \
-    --tx-file tx-proposal.txsigned \
-    "${network[@]}"
+# cardano-cli transaction submit \
+#     --tx-file tx-proposal.txsigned \
+#     "${network[@]}"
+
+if ! cardano-cli transaction submit --tx-file tx-proposal.txsigned "${network[@]}"
+then 
+    ((EPOCH=EPOCH + 1))
+    cardano-cli governance create-update-proposal \
+    --epoch "$EPOCH" \
+    "${PROPOSAL_ARGS[@]}" \
+    "${PROPOSAL_KEY_ARGS[@]}" \
+    --out-file update.proposal
+
+    cardano-cli transaction build \
+        --tx-in "$TXIN" \
+        --change-address "$CHANGE_ADDRESS" \
+        --update-proposal-file update.proposal \
+        --witness-override 3 \
+        "${network[@]}" \
+        --out-file tx-proposal.txbody
+
+    cardano-cli transaction sign \
+        "${network[@]}" \
+        --tx-body-file tx-proposal.txbody \
+        --out-file tx-proposal.txsigned \
+        --signing-key-file $PAYMENT_SIGNING_KEY --address "$CHANGE_ADDRESS" \
+        --signing-key-file bft-leader.skey
+    
+    cardano-cli transaction submit --tx-file tx-proposal.txsigned "${network[@]}"
+fi

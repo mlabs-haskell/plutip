7bc1bcdfcbb3bffbba77541bb3498825c544781d
David Wilson <30732161+Anteproperispomenon@users.noreply.github.com>
2022-08-02 12:55:31 -0400

    Fixed parser usage.
    Now compiles when running `cabal build`. Also works when running `cabal run local-cluster -- --wallets n --wallet-dir /path/to/wallets`.

--- local-cluster/Main.hs
+++ local-cluster/Main.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE DerivingStrategies #-}
 
 module Main (main) where
 
@@ -8,7 +10,7 @@ import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Reader (ReaderT (ReaderT))
 import Data.Default (def)
 import Options.Applicative qualified as Options
-import Options.Applicative (Parser, info, sample, helper)
+import Options.Applicative (Parser, info, helper)
 import Test.Plutip.Config qualified as Config
 import Test.Plutip.Internal.BotPlutusInterface.Wallet (walletPkh, addSomeWalletDir)
 import Test.Plutip.Internal.Types (nodeSocket)
@@ -22,14 +24,14 @@ import Test.Plutip.LocalCluster (
 
 main :: IO ()
 main = do
-  args <- Options.execParser (info (sample <**> helper) pClusterConfig)
+  args <- Options.execParser (info (pClusterConfig <**> helper) mempty)
   (st, _) <- startCluster def $ do
     let nWall = numWallets args
         wPath = dirWallets args
     ws <- replicateM (max 1 nWall) $ addSomeWalletDir [toAda 10000] wPath
     waitSeconds 2 -- let wallet Tx finish, it can take more time with bigger slot length
     separate
-    forM_ (zip ws [1..]) $ \(w,n) -> liftIO $ do
+    forM_ (zip ws [(1 :: Int)..]) $ \(w,n) -> liftIO $ do
       putStrLn $ "Wallet " ++ show n ++ " PKH: " ++ show (walletPkh w)
       putStrLn $ "Wallet " ++ show n ++ " mainnet address: " ++ show (mkMainnetAddress w)
     prtNodeRelatedInfo

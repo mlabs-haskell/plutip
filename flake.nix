6b9bbc74902204ca2c80dc4e2180828a28924333
Mikhail Lazarev <mikhail@mlabs.city>
2022-09-23 15:29:40 +0300

    update to newer bpi

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
       flake = false;
     };
     bot-plutus-interface.url =
-      "github:mlabs-haskell/bot-plutus-interface?ref=vasil-compliant-v1.0.0";
+      "github:mlabs-haskell/bot-plutus-interface?ref=857ec745d50f7f0ebd5cd934110403fae301ef6f";
   };
 
   outputs =

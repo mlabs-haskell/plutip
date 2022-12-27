99e2f38ac4c944ec8a3c058237756c3dc5456983
Mikhail Lazarev <mikhail@mlabs.city>
2022-12-27 21:23:19 +0400

    need fix: fix keys derivation to one in comments

--- src/Test/Plutip/Internal/BotPlutusInterface/Wallet.hs
+++ src/Test/Plutip/Internal/BotPlutusInterface/Wallet.hs
@@ -1,33 +1,39 @@
-module Test.Plutip.Internal.BotPlutusInterface.Wallet (
-  BpiWallet (..),
-  addSomeWallet,
-  addSomeWalletDir,
-  eitherAddSomeWallet,
-  eitherAddSomeWalletDir,
-  mkMainnetAddress,
-  cardanoMainnetAddress,
-  ledgerPaymentPkh,
-  showAddress,
-) where
+module Test.Plutip.Internal.BotPlutusInterface.Wallet
+  ( BpiWallet (..),
+    addSomeWallet,
+    addSomeWalletDir,
+    eitherAddSomeWallet,
+    eitherAddSomeWalletDir,
+    mkMainnetAddress,
+    cardanoMainnetAddress,
+    ledgerPaymentPkh,
+    showAddress,
+  )
+where
 
-import Cardano.Api (AddressAny, PaymentKey, SigningKey, VerificationKey)
+import Cardano.Api (AddressAny, PaymentExtendedKey, PaymentKey, SigningKey (PaymentExtendedSigningKey), VerificationKey, castVerificationKey)
 import Cardano.Api qualified as CAPI
 import Cardano.BM.Data.Tracer (nullTracer)
-import Cardano.Wallet.Primitive.Types.Coin (Coin (Coin))
-
 -- import Cardano.Wallet.Shelley.Launch.Cluster (
 --   sendFaucetFundsTo,
 --  )
-import Test.Plutip.Internal.Cluster (
-  sendFaucetFundsTo,
- )
 
-import Control.Arrow (ArrowChoice (left))
+import Cardano.Crypto.Seed qualified as Crypto
+import Cardano.Mnemonic (someMnemonicToBytes)
+import Cardano.Mnemonic qualified as CADDR
+-- import Cardano.Wallet.Primitive.AddressDerivation.Shared
+--   ( SharedKey (getKey),
+--     generateKeyFromSeed,
+--   )
+import Cardano.Wallet.Primitive.Types.Coin (Coin (Coin))
+import Control.Arrow (ArrowChoice (left), (>>>))
 import Control.Monad (void)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.Reader (ReaderT, ask)
 import Data.Aeson.Extras (encodeByteString)
 import Data.Bool (bool)
+import Data.ByteArray (convert)
+import Data.Text (Text)
 import Data.Text qualified as Text
 import Ledger (PaymentPubKeyHash (PaymentPubKeyHash), PubKeyHash (PubKeyHash))
 import Numeric.Positive (Positive)
@@ -37,15 +43,23 @@ import System.Directory (createDirectoryIfMissing)
 import System.FilePath ((<.>), (</>))
 import Test.Plutip.Internal.BotPlutusInterface.Setup qualified as Setup
 import Test.Plutip.Internal.BotPlutusInterface.Types (BpiError (BotInterfaceDirMissing, SignKeySaveError))
+import Test.Plutip.Internal.Cluster
+  ( sendFaucetFundsTo,
+  )
 import Test.Plutip.Internal.Types (ClusterEnv, nodeSocket, supportDir)
+import Cardano.Wallet.Primitive.AddressDerivation.Shelley (generateKeyFromSeed, ShelleyKey (getKey, ShelleyKey))
+import Cardano.Address.Derivation (deriveXPrv, DerivationScheme (DerivationScheme2), Index, DerivationType (Hardened, Soft), indexFromWord32, toXPub)
+import Data.Maybe
+import Cardano.Api.Shelley (VerificationKey(PaymentExtendedVerificationKey))
+import Cardano.Address (paymentAddress)
 
 -- | Wallet that can be used by bot interface,
 --  backed by `.skey` file when added to cluster with `addSomeWallet`
 data BpiWallet = BpiWallet
-  { walletPkh :: !PubKeyHash
-  , vrfKey :: VerificationKey PaymentKey
-  , signKey :: SigningKey PaymentKey
-  -- todo: do we need something else?
+  { walletPkh :: !PubKeyHash,
+    vrfKey :: VerificationKey PaymentKey,
+    signKey :: SigningKey PaymentKey
+    -- todo: do we need something else?
   }
   deriving stock (Show)
 
@@ -146,3 +160,67 @@ showAddress = Text.unpack . CAPI.serialiseAddress
 
 ledgerPaymentPkh :: BpiWallet -> PaymentPubKeyHash
 ledgerPaymentPkh = PaymentPubKeyHash . walletPkh
+
+-- 1852H/1815H/0H/0/0
+
+addMnemonicWallet :: MonadIO m => [Positive] -> [Text] -> ReaderT ClusterEnv m (Either BpiError ExtWallet)
+addMnemonicWallet funds mnemonic = do
+  let someMnem =
+        either (error . show) id (CADDR.mkSomeMnemonic @'[12] mnemonic)
+      xPrv = getKey $ generateKeyFromSeed (someMnem, Nothing) mempty
+      rootExtKey = PaymentExtendedSigningKey xPrv
+      hIx = fromJust . indexFromWord32 @(Index 'Hardened _) . fromInteger
+      sIx = fromJust . indexFromWord32 @(Index 'Soft _) . fromInteger
+      derivePrv ix prv = deriveXPrv DerivationScheme2 prv ix
+      deriveChild = -- FIXME: not correct
+        derivePrv (hIx 1852)
+        >>> derivePrv (hIx 1815)
+        >>> derivePrv (hIx 0)
+        >>> derivePrv (sIx 0)
+        >>> derivePrv (sIx 0)
+
+      addrSKey = PaymentExtendedSigningKey $ deriveChild xPrv
+      addrVKey = castVerificationKey $ PaymentExtendedVerificationKey $ toXPub xPrv
+    
+
+     
+
+  return . Right $ ExtWallet rootExtKey addrSKey addrVKey
+
+data ExtWallet = ExtWallet
+  { extRootKey :: SigningKey PaymentExtendedKey
+  , addressSignKey :: SigningKey PaymentExtendedKey
+  , addressVerKey ::  VerificationKey PaymentKey
+  }
+
+seedFromMnemonic :: [Text] -> Crypto.Seed
+seedFromMnemonic mnemonic =
+  let someMnem =
+        either (error . show) id (CADDR.mkSomeMnemonic @'[12] mnemonic)
+   in Crypto.mkSeedFromBytes $ convert $ someMnemonicToBytes someMnem
+
+{-
+genShelleyAddresses :: SomeMnemonic -> [Address]
+genShelleyAddresses mw =
+    let
+        (seed, pwd) =
+            (mw, Passphrase "encryption"
+forall a. Monoid a => a
+mempty)
+        rootXPrv =
+            Shelley.generateKeyFromSeed (seed, Nothing) pwd
+        accXPrv =
+            deriveAccountPrivateKey pwd rootXPrv minBound
+        addrXPrv =
+            deriveAddressPrivateKey pwd accXPrv UtxoExternal
+    in
+        [ paymentAddress @'Mainnet $ publicKey $ addrXPrv ix
+        | ix <- [minBound..maxBound]
+        ]
+
+-------------
+
+paymentAddress :: NetworkDiscriminant key -> key 'PaymentK XPub -> Address
+
+
+-}

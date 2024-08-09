ab1b02ce19fbcabcb3e9bc8d1df06610d7e88f4c
Hongrui Fang <chfanghr@gmail.com>
2024-08-09 19:59:31 +0800

    build and deploy doc of plutip-rust

--- hercules-ci.nix
+++ hercules-ci.nix
@@ -17,5 +17,19 @@
     };
   };
 
+  hercules-ci.github-pages.branch = "master";
+
+  perSystem = { config, pkgs, ... }: {
+    hercules-ci.github-pages.settings.contents = pkgs.stdenv.mkDerivation {
+      name = "plutip-rust-github-pages";
+      src = config.packages.plutip-rust-rust-doc;
+      buildPhase = ''
+        mkdir $out
+        cp -r -L -v $src/share/doc/* $out/
+        echo '<meta http-equiv="refresh" content="0; url=plutip_rust">' > $out/index.html
+      '';
+    };
+  };
+
   herculesCI.ciSystems = [ "x86_64-linux" "x86_64-darwin" ];
 }
